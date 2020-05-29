3e902851bf4203cf5a93ec818ce4bdefed38e7cb
Elizabeth Harper <foxcapade@gmail.com>
2020-05-29 13:37:48 -0400

    improved histogram buckets for external command metric

--- internal/command/metric.go
+++ internal/command/metric.go
@@ -10,6 +10,6 @@ var (
 		Namespace: "cmd",
 		Name:      "execution_time",
 		Help:      "Execution time for external command calls in milliseconds.",
-		Buckets:   []float64{1, 10, 50, 100, 500, 1000, 5000},
+		Buckets:   []float64{10, 50, 100, 500, 1000, 5000, 10_000},
 	}, []string{"command"})
 )
