74979516ad558fc108ba5f87baa6919bb995e9d1
Elizabeth Harper <foxcapade@gmail.com>
2020-06-08 11:50:25 -0400

    documentation

--- internal/util/archive/unpack.go
+++ internal/util/archive/unpack.go
@@ -11,13 +11,17 @@ import (
 )
 
 func unpack(dir, file string, cmd *exec.Cmd) ([]string, error) {
+	// Get the list of files that were in the directory before we unpack the
+	// archive.  This will be used later to figure out what we actually unpacked.
 	before, err := fs.ListFiles(dir)
 	if err != nil {
 		return nil, err
 	}
 
+	// Set the command working directory to the given dir.
 	cmd.Dir = dir
 
+	// Run & time the unpack command for metrics.
 	time, err := util.TimeCmd(cmd)
 	metrics.RecordCommandTime(cmd.Args[0], time)
 
@@ -25,16 +29,20 @@ func unpack(dir, file string, cmd *exec.Cmd) ([]string, error) {
 		return nil, err
 	}
 
+	// Remove the archive file itself
 	err = os.Remove(file)
 	if err != nil {
 		return nil, err
 	}
 
+	// List the files in the directory after the unpack.
 	after, err := fs.ListFiles(dir)
 	if err != nil {
 		return nil, err
 	}
 
+	// Make a new list containing only entries that exist in the directory now and
+	// were not in the directory last time we checked.
 	out := make([]string, 0, len(after) - len(before))
 	for i := range after {
 		if !slice.ContainsString(before, after[i]) {
@@ -42,6 +50,7 @@ func unpack(dir, file string, cmd *exec.Cmd) ([]string, error) {
 		}
 	}
 
+	// Sort the output list by name ascending
 	sort.Slice(out, func(i, j int) bool {
 		return out[i] < out[j]
 	})
