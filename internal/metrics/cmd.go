bc78d959c10f7cf89c0b8c2bfea8d2981cb5c133
Elizabeth Harper <foxcapade@gmail.com>
2020-06-26 13:13:56 -0400

    updated handler timing histogram

--- internal/metrics/cmd.go
+++ internal/metrics/cmd.go
@@ -11,12 +11,13 @@ var (
 			Namespace: "cmd",
 			Name:      "execution_time",
 			Help:      "Execution time for external command calls in milliseconds.",
-			Buckets:   []float64{10, 50, 100, 500, 1000, 5000, 10_000},
+			// 50ms, 100ms, 1s, 10s, 1m, 5m, 10m
+			Buckets: []float64{50, 100, 1000, 10_000, 60_000, 300_000, 600_000},
 		},
 		[]string{"command"},
 	)
 )
 
-func RecordCommandTime(cmd string, time float64){
+func RecordCommandTime(cmd string, time float64) {
 	promCommandTime.WithLabelValues(cmd).Observe(time)
 }
